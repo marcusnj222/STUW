15db9fff9f428b3852956420a94827d68643d07a
Tony Love <tonylove@tony-love.home>
2016-07-08 18:51:05 -0400

    Initial Commit

---
+++ StuW/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  StuW
+//
+//  Created by Tony Love on 7/8/16.
+//  Copyright © 2016 Tony Love. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ StuW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StuW
+//
+//  Created by Tony Love on 7/8/16.
+//  Copyright © 2016 Tony Love. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
